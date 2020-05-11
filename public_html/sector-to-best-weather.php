d9b168d38be1e060d135db50d94731db3792df2b
Hanford Neild <hneild1@ugradx.cs.jhu.edu>
2020-05-11 17:14:00 -0400

    Initial commit

---
+++ public_html/sector-to-best-weather.php
@@ -0,0 +1,103 @@
+ <head>
+	<title>Show Raw Scores For One Student</title>
+ </head>
+ <body>
+ <?php
+
+
+function outputResultsTableHeader() {
+    echo "<tr>";
+    echo "<th> SECTOR </th>";
+    echo "<th> PERCENT GAIN </th>";
+    echo "<th> TEMP BRACKET </th>";
+    echo "</tr>";
+}
+
+
+// Open a database connection
+// The call below relies on files named open.php and dbase-conf.php
+// It initializes a variable named $mysqli, which we use below
+include 'open.php';
+
+// Configure error reporting settings
+ini_set('error_reporting', E_ALL); // report errors of all types
+ini_set('display_errors', true);   // report errors to screen (don't hide from user)
+
+// Collect the data input posted here from the calling page
+// The associative array called S_POST stores data using names as indices
+if(isset($_POST['LIMIT'])){
+    $LIMIT = $_POST['LIMIT'];
+}else{
+    $LIMIT = 10;
+}
+
+// Call the stored procedure named ShowRawScores
+// "multi_query" executes given (multiple-statement) MySQL query
+// It returns true if first statement executed successfully; false otherwise.
+// Results of first statement are retrieved via $mysqli->store_result()
+// from which we can call ->fetch_row() to see successive rows
+if ($mysqli->multi_query("SELECT SectorName ,AVG((100*(T.ClosePrice-T.OpenPrice)/T.OpenPrice)) AS GAIN, FLOOR((MaxTemp+MinTemp)/20)*10 AS TempBracket FROM TRADES AS T INNER JOIN FORECAST AS F ON T.dateID = F.dateID INNER JOIN SECURITIES AS S ON S.symbol = T.symbol INNER JOIN SECTOR ON SECTOR.ID = S.SectorID GROUP BY SectorName, TempBracket ORDER BY GAIN DESC;")) {
+
+
+    // Check if a result was returned after the call
+    if ($result = $mysqli->store_result()) {
+
+        $numResults = mysqli_num_rows($result);
+        if($numResults>0) {
+        
+            echo "<table border=\"1px solid black\">";
+            $row = $result->fetch_row();
+
+            // If the first row of result begins with 'ERROR: ', then our
+            // stored procedure produced a relation that indicates error(s)
+            if (strcmp($row[0], 'ERROR: ') == 0) {
+                echo "<tr><th> Result </th></tr>";
+                do {
+                    echo "<tr><td>" ;
+                    for($i = 0; $i < sizeof($row); $i++){
+                        echo $row[$i];
+                    }
+                    echo "</td></tr>";
+                } while ($row = $result->fetch_row());
+
+            // Otherwise, we received real results, so output table
+            } else {
+                
+                // Output appropriate table header row
+                outputResultsTableHeader();
+                $counter = 0;
+                // Output each row of resulting relation
+                do {
+                    $counter = $counter + 1;
+                    echo "<tr>";
+                    for($i = 0; $i < sizeof($row); $i++){
+                        echo "<td>" . $row[$i] . "</td>";
+                    }
+                    echo "</tr>";
+
+                    if($counter>=$LIMIT){
+                        break;
+                    }
+                } while ($row = $result->fetch_row());
+            }
+            echo "</table>";
+            $result->close();
+        }else{
+            echo "<h1>We do not have data for that date!</h1>";
+        }
+    }
+
+// The "multi_query" call did not end successfully, so report the error
+// This might indicate we've called a stored procedure that does not exist,
+// or that database connection is broken
+} else {
+        printf("<br>Error: %s\n", $mysqli->error);
+}
+
+// Close the connection created above by including 'open.php' at top of this file
+mysqli_close($mysqli);
+
+
+ ?>
+ </body>
+
