9c2dde3464df804a51efff4e0dea7f385d20446a
Ahmed Elzokm <aelzokm1@ugradx.cs.jhu.edu>
2020-05-11 17:05:23 -0400

    latest

--- public_html/company-rain.php
+++ public_html/company-rain.php
@@ -5,7 +5,8 @@
 <?php
 function outputResultsTableHeader() {
     echo "<tr>";
-    echo "<th> Sector </th>";
+    echo "<th> Company Name </th>";
+    echo "<th> Symbol </th>";
     echo "<th> Avg Gain </th>";
     echo "<th> Volume Traded in Season </th>";
     echo "</tr>";
@@ -46,8 +47,11 @@ if ($Seasons == "Fall") {
 
 
 
+
+
+
 //second query is to find all the stock prices for those dates and avergae them.
-$sql= "SELECT SectorName, AVG(Gain) AS STOTAL, SUM(Volumes) FROM SECTOR JOIN (SELECT SectorID, SECURITIES.Symbol, Gain, Volumes FROM SECURITIES JOIN(SELECT Symbol, AVG(100*(TRADES.ClosePrice - TRADES.OpenPrice)/TRADES.OpenPrice) AS Gain, Sum(Volume) AS Volumes FROM TRADES WHERE DateID IN (".$rSeason.") GROUP BY Symbol)AS Calc on SECURITIES.Symbol = Calc.Symbol ORDER BY Gain DESC) AS Calc2 on SECTOR.ID = Calc2.SectorID ORDER BY STOTAL DESC;";
+$sql= "SELECT CompanyName, SECURITIES.Symbol, Gain, Volumes FROM SECURITIES JOIN(SELECT Symbol, AVG(100*(TRADES.ClosePrice - TRADES.OpenPrice)/TRADES.OpenPrice) AS Gain, Sum(Volume) AS Volumes FROM TRADES WHERE DateID IN (".$rSeason.") GROUP BY Symbol)AS Calc on SECURITIES.Symbol = Calc.Symbol ORDER BY Gain DESC;";
 if ($mysqli->multi_query($sql)) {
     // Check if a result was returned after the call
     if ($result = $mysqli->store_result()) {
